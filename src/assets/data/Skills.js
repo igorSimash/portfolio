b88f6a71a413b9015f1609c3749f90db0c4df119
igorSimash <vigorochok@gmail.com>
2023-01-27 13:17:23 +0200

    added skills

--- src/assets/data/Skills.js
+++ src/assets/data/Skills.js
@@ -1,34 +1,46 @@
 export const skills = [
     {
-        skill: 'HTML',
-        percentage: 85,
-    },
-    {
-        skill: 'CSS',
-        percentage: 80,
-    },
-    {
         skill: 'React',
-        percentage: 80,
+        percentage: 89,
+    },
+    {
+        skill: 'Redux',
+        percentage: 85,
     },
     {
         skill: 'Javascript',
-        percentage: 85,
+        percentage: 92,
     },
     {
         skill: 'Typescript',
-        percentage: 70,
+        percentage: 88,
+    },
+    {
+        skill: 'HTML',
+        percentage: 93,
+    },
+    {
+        skill: 'CSS',
+        percentage: 90,
     },
     {
         skill: 'TailwindCSS',
-        percentage: 85,
+        percentage: 87,
     },
     {
         skill: 'SASS',
-        percentage: 80,
+        percentage: 85,
     },
     {
-        skill: 'Bootstrap',
-        percentage: 65,
+        skill: 'SQL',
+        percentage: 91,
+    },
+    {
+        skill: 'Node.js',
+        percentage: 78,
+    },
+    {
+        skill: 'Express',
+        percentage: 74,
     },
 ]
